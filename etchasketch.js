ced7279e59667381424e5aafdeff67a3a4c31588
polakenfold <polakenfold@gmail.com>
2023-01-31 01:50:49 -0300

    start

---
+++ etchasketch.js
@@ -0,0 +1,15 @@
+// // select body
+// const bod = document.body;
+// bod.textContent = 'Hola'
+const bod = document.body;
+const header = document.createElement('h1');
+header.classList.add('header');
+header.style.cssText = ""; //put some style here
+
+
+// crear h1 EAS
+
+// crear div grid container
+    // crear grilla 16x16
+
+
